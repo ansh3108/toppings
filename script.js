cf64a2cd537d164b592602a740f47784fbb35d97
ansh3108 <anshkumar9686@gmail.com>
2025-05-19 16:25:38 +0530

    cinn

---
+++ script.js
@@ -0,0 +1,15 @@
+const moodButtons = document.querySelectorAll('button')
+const body = document.body
+
+const moods = {
+  chill: 'linear-gradient(to right, #4facfe, #00f2fe)',
+  hype: 'linear-gradient(to right, #ff416c, #ff4b2b)',
+  sad: 'linear-gradient(to right, #373B44, #4286f4)'
+}
+
+moodButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const selectedMood = btn.dataset.mood
+    body.style.background = moods[selectedMood]
+  })
+})
